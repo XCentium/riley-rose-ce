897f38dbe1484eee91481650365b8952c757f87b
olaowolawi <ola.owolawi@xcentium.com>
2017-12-08 09:59:53 -0800

    Updated Vertex tax component model

--- Plugin.Xcentium.RileyRose.Tax/Components/VertexTax.cs
+++ Plugin.Xcentium.RileyRose.Tax/Components/VertexTax.cs
@@ -1,13 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Sitecore.Commerce.Core;
+﻿using Sitecore.Commerce.Core;
 
 namespace Plugin.Xcentium.RileyRose.Tax.Components
 {
     public class VertexTax : Component
     {
+        /// <summary>
+        /// Unit Price tax based off
+        /// </summary>
+        public decimal UnitPrice { get; set; }
+
+        /// <summary>
+        /// Quantity applied to the unit price
+        /// </summary>
+        public int Quantity { get; set; }
+
+        /// <summary>
+        /// Line level discount
+        /// </summary>
+        public decimal LineDiscount { get; set; }
+
+        /// <summary>
+        /// Cart level discount
+        /// </summary>
+        public decimal CartLevelDiscount { get; set; }
+
+        /// <summary>
+        /// Total discount applied to the cart line
+        /// </summary>
+        public decimal TotalDiscount { get; set; }
+
+        /// <summary>
+        /// Grand total for tax purposes
+        /// </summary>
+        public decimal GrandPricePrice { get; set; }
+
+
+        /// <summary>
+        /// The Tax from Responsys
+        /// </summary>
         public decimal Tax { get; set; }
     }
 }
